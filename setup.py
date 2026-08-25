47db51db3c47e0a5e8adc6d59c05bdcdb6ade274
RamyaKGit <karri.ramya@gmail.com>
2026-08-25 10:37:16 -0700

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools.command import rotate
+from os import name
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+    return requirements
+
+
+setup(
+name = 'mlprojects',
+version='0.0.1',
+author='Ramya',
+packages=find_packages(),
+install_requires= get_requirements('requirements.txt')
+
+
+
+)
