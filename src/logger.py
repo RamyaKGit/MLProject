13f3f7ad87ec9332e8035d219049f5312c785d88
RamyaKGit <karri.ramya@gmail.com>
2026-09-04 12:08:53 -0700

    logs will be added to console

--- src/logger.py
+++ src/logger.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import sys
 from datetime import datetime
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
@@ -8,8 +9,12 @@ os.makedirs(LOG_PATH,exist_ok=True)
 LOG_FILE_PATH = os.path.join(LOG_PATH,LOG_FILE)
 
 logging.basicConfig(
-    filename=LOG_FILE_PATH,
+    handlers=[
+        logging.FileHandler(LOG_FILE_PATH),
+        logging.StreamHandler(sys.stdout)
+    ],
     format='[%(asctime)s] %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+
     
